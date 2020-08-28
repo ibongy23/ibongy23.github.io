176fa11faa3807514d745b6f0edef7a7706ff85a
Ibon Godoy <bonchis.gy.27@gmail.com>
2020-08-27 23:33:58 -0500

    Modifique conversor de monedas, agregue pesos mexicanos y pesos argentinos

--- Curso python/codigo_python/conversor.py
+++ Curso python/codigo_python/conversor.py
@@ -1,10 +1,42 @@
-pesos = input("¿cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3768
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas📊🗳
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elije una opción : """
+
+opcion = int(input (menu))
+if opcion == 1 :
+    pesos = input("¿cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3768
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion ==2:
+    pesos = input("¿cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 73.88
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 3:
+    pesos = input("¿cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 22.09
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else: 
+    print('Ingrese uan opción correcta por favor')
+
+
+
 
 dolares1 = input("¿Cuántos dolares estadounidenses tienes?: ")
 dolares1 = float(dolares1)
